581be400efb45160440422c0243d03c2690362ea
wiktor.widera <wiktor.widera@gmail.com>
2023-04-30 14:09:23 +0200

    Modifying the RockPaperScissors class

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,4 +1,8 @@
 public class RockPaperScissors {
     public static void main(String[] args) {
+        GameHelper gameHelper = new GameHelper();
+
+        gameHelper.initializeGame();
+        gameHelper.summarizeGame();
     }
 }
